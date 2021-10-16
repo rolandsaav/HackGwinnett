cfee8240114bbe5bbc30ecac09c05f03d972585f
Roland Saavedra <rolandsaavedra22@gmail.com>
2021-10-16 16:45:30 -0400

    works

--- frontend/src/components/CauseView.js
+++ frontend/src/components/CauseView.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 import CauseScroll from './CauseScroll'
+import { db } from '../firebase'
 
 export default function CauseView() {
     const [selectedCause, setSelectedCause] = useState({})
+
 
     return (
         <Container fluid>
@@ -18,8 +20,11 @@ export default function CauseView() {
                     <Card className="text-center mt-5">
                         <Card.Body>
                             <Card.Title> {selectedCause.name} </Card.Title>
-                        <h1>{selectedCause.name}</h1>
-                        <h3>{selectedCause.desc}</h3>
+                            <h1>{selectedCause.name}</h1>
+                            <h3>{selectedCause.desc}</h3>
+                            <Button variant="primary" size="lg" type="button" >
+                            Join this cause
+                            </Button>
                         </Card.Body>
                     </Card>
                     }
